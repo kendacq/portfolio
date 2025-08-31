6b9f7d882c614ac41ae424d32cdfa73826b076ab
kendacq <kenshin.dacquigan@gmail.com>
2025-09-01 00:47:11 +0800

    disable parallax in javascript

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+const screenWidthThreshold = 768;
+
 document.addEventListener('mousemove', function (e) {
-    const background = document.querySelector('.background');
-    const foreground = document.querySelector('.foreground');
+    if (window.innerWidth > screenWidthThreshold) {
+        const background = document.querySelector('.background');
+        const foreground = document.querySelector('.foreground');
 
-    const mouseX = e.clientX;
-    const mouseY = e.clientY;
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
 
-    background.style.transform = `translate(${(mouseX - window.innerWidth / 2) / 100}px, ${(mouseY - window.innerHeight / 2) / 20}px)`;
-    foreground.style.transform = `translate(${(mouseX - window.innerWidth / 2) / 80}px, ${(mouseY - window.innerHeight / 2) / 10}px)`;
+        background.style.transform = `translate(${(mouseX - window.innerWidth / 2) / 100}px, ${(mouseY - window.innerHeight / 2) / 20}px)`;
+        foreground.style.transform = `translate(${(mouseX - window.innerWidth / 2) / 80}px, ${(mouseY - window.innerHeight / 2) / 10}px)`;
+    }
 });
